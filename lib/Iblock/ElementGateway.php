ff38dc19308140f551653eeacdeae012bc57443d
mediaca <mediaca@ya.ru>
2025-01-30 20:20:22 +0300

    add: шлюз получения данных элемента

---
+++ lib/Iblock/ElementGateway.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mediaca\Crossposting\Iblock;
+
+use Bitrix\Main\Loader;
+use CIBlockElement;
+
+class ElementGateway
+{
+    public function __construct()
+    {
+        Loader::requireModule('iblock');
+    }
+
+    public function getElement(array $select, array $filter): ?array
+    {
+        $select = array_merge(
+            $select,
+            [
+                'ID',
+                'IBLOCK_ID',
+                'DETAIL_PAGE_URL',
+            ],
+        );
+
+        $result = CIBlockElement::GetList([], $filter, false, ['nPageSize' => 1], $select);
+
+        return $result->GetNext(false, false);
+    }
+}

1a8dde5c419ea11788a98267c05e7f05d288ac8c
mediaca <mediaca@ya.ru>
2025-01-31 11:02:01 +0300

    add: шлюз обновления статуса таска

--- lib/Task/TaskGateway.php
+++ lib/Task/TaskGateway.php
@@ -10,15 +10,29 @@ class TaskGateway
 {
     public static function add(DateTime $created, int $elementId, Channel $channel): void
     {
-        $data = [
+        $result = TaskTable::add([
             'CREATED' => $created,
             'ELEMENT_ID' => $elementId,
             'CHANNEL' => $channel->value,
-        ];
+        ]);
 
-        $result = TaskTable::add($data);
         if (!$result->isSuccess()) {
             throw new \DomainException('Error adding task: ' . implode(', ', $result->getErrorMessages()));
+        }
+    }
+
+    public static function updateStatus(int $id, Status $status, DateTime $dateExec): void
+    {
+        $result = TaskTable::update(
+            $id,
+            [
+                'STATUS' => $status->value,
+                'DATE_EXEC' => $dateExec,
+            ],
+        );
+
+        if (!$result->isSuccess()) {
+            throw new \DomainException('Error updating task: ' . implode(', ', $result->getErrorMessages()));
         }
     }
 }
